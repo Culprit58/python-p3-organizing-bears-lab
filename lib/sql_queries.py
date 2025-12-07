5f35b14319715244caa48f3f049241767d887643
Enock Cheruiyot <cheruiyote518@gmail.com>
2025-12-07 10:50:31 +0300

    lab done

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,37 @@
+# Select all female bears and return name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE sex = 'F';
 """
 
+# Select all bear names in alphabetical order
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name
+FROM bears
+ORDER BY name ASC;
 """
 
+# Select all bears that are alive and return name and age, youngest to oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE alive = 1
+ORDER BY age ASC;
 """
 
+# Select the oldest bear and return name and age
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+ORDER BY age DESC
+LIMIT 1;
 """
+
+# Select the youngest bear and return name and age
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+ORDER BY age ASC
+LIMIT 1;
 """
